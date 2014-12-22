8bb7d76dd9cadade2d95c6c7ffba82da9e5fa121
OnurIltan <onur.iltan@ozu.edu.tr>
2014-12-22 18:43:02 +0200

    Player1 class and imageloader class addded

---
+++ src/main/Player1.java
@@ -0,0 +1,72 @@
+package main;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+import entities.EntityBrick;
+
+
+public class Player1 extends GameObject implements EntityBrick {
+
+	
+
+	private double velX = 0;
+	private double velY = 0;
+	
+	private BufferedImage player1;
+
+	public Player1(double x, double y, Game g){
+		super(x,y);
+		Tank tank = new Tank(g.getTank());
+		player1 = tank.grabImage();
+		
+	}
+	
+	public void tick(){
+		x+= velX;
+		y+= velY;
+		if(x <= 0) x = 0;
+		if(x >= 640-18) x = 640-18;
+		if(y <= 0) y = 0;
+		if(y >= 480-32) y = 480-32;
+		
+				
+	}
+	public void render(Graphics g){
+		g.drawImage(player1, (int) x, (int) y, null);
+	}
+	
+	public double getX(){
+		return x;
+		
+	}
+	public double getY(){
+		return y;
+		
+	}
+	
+	public void setX(double x){
+		this.x = x;
+		
+	}
+	public void setY(double y){
+		this.y = y; 
+		
+	}
+	public void setvelX(double velX){
+		this.velX = velX;
+		
+	}
+	public void setvelY(double velY){
+		this.velY = velY; 
+		
+	}
+
+	@Override
+	public Rectangle getBounds(){
+		return new Rectangle((int)x, (int)y, 38, 18);
+	}
+
+
+}
